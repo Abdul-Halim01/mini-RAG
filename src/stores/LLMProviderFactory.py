d7875ee449666ee30e75f7ab294bba892b0e64e0
Abdul-Halim01 <eng.abdulhalem@gmail.com>
2026-02-17 19:35:56 +0200

    Implement LLmProvderFactory with Essential functions

---
+++ src/stores/LLMProviderFactory.py
@@ -0,0 +1,40 @@
+from ..LLMEnums import LLMEnums
+from ..LLMInterface import LLMInterface
+from ..providers.OpenAIProvider import OpenAIProvider
+
+
+
+class LLMProviderFactory:
+
+    def __init__(self,config:dict):
+        self.config=config
+    
+
+    def create(self,provider:str):
+        if provider == LLMEnums.OPENAI.value:
+            return OpenAIProvider(
+                api_key=self.config.OPENAI_API_KEY,
+                api_url=self.config.OPENAI_API_URL,
+                default_input_max_characters=self.config.INPUT_DEFAULT_MAX_CHARACTER,
+                default_generation_max_characters=self.config.GENERATION_DEFAULT_MAX_TOKENS,
+                default_temperature=self.config.GENERATION_DEFAULT_TEMPREATURE
+            )
+
+        if provider == LLMEnums.GEMINI.value:
+            pass
+
+        if provider == LLMEnums.COHERE.value:
+            return CoHereProvider(
+                api_key=self.config.COHERE_API_KEY,
+                default_input_max_characters=self.config.INPUT_DEFAULT_MAX_CHARACTER,
+                default_generation_max_characters=self.config.GENERATION_DEFAULT_MAX_TOKENS,
+                default_temperature=self.config.GENERATION_DEFAULT_TEMPREATURE
+            )
+
+
+
+
+    @staticmethod
+    def get_provider(provider_name:str):
+        if provider_name==LLMEnums.OPENAI.value:
+            return OpenAIProvider()
