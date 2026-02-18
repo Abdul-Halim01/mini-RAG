afa6bdd3454d21026ac0f8af00f16a194cedc855
Abdul-Halim01 <eng.abdulhalem@gmail.com>
2026-02-19 01:56:31 +0200

    Adding NLPController

---
+++ src/controllers/__init__.py
@@ -0,0 +1,4 @@
+from .DataControllers import DataController
+from .ProjectController import ProjectController
+from .ProcessController import ProcessController
+from .NLPController import NLPController
